0f153b2239f31c9854e55ef888ff779f0350d293
DavidGoldman <dallasftball@gmail.com>
2014-12-28 22:57:51 -0500

    Basic API

---
+++ InspectiveC.h
@@ -0,0 +1,26 @@
+#ifndef INSPECTIVE_C_H
+#define INSPECTIVE_C_H
+
+#include <objc/objc.h>
+
+#if __cplusplus
+extern "C" {
+#endif
+  // Watches/unwatches the specified object. Objects will be automatically unwatched when they
+  // receive a -|dealloc| message.
+  void InspectiveC_watchObject(id obj);
+  void InspectiveC_unwatchObject(id obj);
+
+  // Watches/unwatches instances of the specified class ONLY - will not watch subclass instances.
+  void InspectiveC_watchInstancesOfClass(Class clazz);
+  void InspectiveC_unwatchInstancesOfClass(Class clazz);
+
+  // Watches/unwatches the specified selector.
+  void InspectiveC_watchSelector(SEL _cmd);
+  void InspectiveC_unwatchSelector(SEL _cmd);
+
+#if __cplusplus
+}
+#endif
+
+#endif
