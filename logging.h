fc189ce47e50d6820f3f3dbb804228fcf024db4e
DavidGoldman <dallasftball@gmail.com>
2015-06-02 21:28:09 -0700

    Macro hackz for logging

--- logging.h
+++ logging.h
@@ -5,15 +5,31 @@
 #include <cstdio>
 
 #ifdef __arm64__
+#define arg_list pa_list
+#define int_up_cast(t) t
+#define uint_up_cast(t) t
 #include "ARM64Types.h"
+#else
+#define arg_list va_list
+#define int_up_cast(t) int
+#define uint_up_cast(t) unsigned int
+#define pa_arg(args, type) va_arg(args, type)
+#define pa_float(args) float_from_va_list(args)
+#define pa_double(args) va_arg(args, double)
+
+#define pa_two_ints(args, varType, varName, intType) \
+  varType varName = va_arg(args, varType); \
+
+#define pa_two_doubles(args, t, varName) \
+  t varName = va_arg(args, t); \
+
+#define pa_four_doubles(args, t, varName) \
+  t varName = va_arg(args, t); \
+
 #endif
 
 void logObject(FILE *file, id obj);
 
-#ifdef __arm64__
-bool logArgument(FILE *file, const char *type, pa_list &args);
-#else
-bool logArgument(FILE *file, const char *type, va_list &args);
-#endif
+bool logArgument(FILE *file, const char *type, arg_list &args);
 
 #endif
--- logging.mm
+++ logging.mm
@@ -45,9 +45,22 @@ void logObject(FILE *file, id obj) {
   fprintf(file, "<%s@%p>", class_getName(kind), reinterpret_cast<void *>(obj));
 }
 
-#ifdef __arm64__
+#ifndef __arm64__
 
-bool logArgument(FILE *file, const char *type, pa_list &args) {
+static float float_from_va_list(va_list &args) {
+  union {
+    uint32_t i;
+    float f;
+  } value = {va_arg(args, uint32_t)};
+  return value.f;
+}
+
+#endif
+
+// Heavily based/taken from AspectiveC by saurik.
+// @see http://svn.saurik.com/repos/menes/trunk/aspectivec/AspectiveC.mm
+// @see https://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/ObjCRuntimeGuide/Articles/ocrtTypeEncodings.html
+bool logArgument(FILE *file, const char *type, arg_list &args) {
   loop:
     switch(*type) {
       case '#': // A class object (Class).
@@ -76,23 +89,23 @@ bool logArgument(FILE *file, const char *type, pa_list &args) {
         }
       } break;
       case 'B': { // A C++ bool or a C99 _Bool.
-        bool value = pa_arg(args, bool);
+        bool value = pa_arg(args, int_up_cast(bool));
         fprintf(file, "%s", value ? "true" : "false");
       } break;
       case 'c': { // A char.
-        signed char value = pa_arg(args, char);
+        signed char value = pa_arg(args, int_up_cast(char));
         fprintf(file, "%d", value);
       } break;
       case 'C': { // An unsigned char.
-        unsigned char value = pa_arg(args, unsigned char);
+        unsigned char value = pa_arg(args, uint_up_cast(unsigned char));
         fprintf(file, "%d", value);
       } break;
       case 's': { // A short.
-        short value = pa_arg(args, short);
+        short value = pa_arg(args, int_up_cast(short));
         fprintf(file, "%d", value);
       } break;
       case 'S': { // An unsigned short.
-        unsigned short value = pa_arg(args, unsigned short);
+        unsigned short value = pa_arg(args, uint_up_cast(unsigned short));
         fprintf(file, "%u", value);
       } break;
       case 'i': { // An int.
@@ -133,8 +146,13 @@ bool logArgument(FILE *file, const char *type, pa_list &args) {
       } break;
       case '{': { // A struct. We check for some common structs.
         if (strncmp(type, "{CGAffineTransform=", 19) == 0) {
+#ifdef __arm64__
           CGAffineTransform *ptr = (CGAffineTransform *)pa_arg(args, void *);
           logNSStringForStruct(file, NSStringFromCGAffineTransform(*ptr));
+#else
+          CGAffineTransform at = va_arg(args, CGAffineTransform);
+          logNSStringForStruct(file, NSStringFromCGAffineTransform(at));
+#endif
         } else if (strncmp(type, "{CGPoint=", 9) == 0) {
           pa_two_doubles(args, CGPoint, point)
           logNSStringForStruct(file, NSStringFromCGPoint(point));
@@ -172,135 +190,3 @@ bool logArgument(FILE *file, const char *type, pa_list &args) {
     }
     return true;
 }
-#else // arm32
-// Heavily based/taken from AspectiveC by saurik.
-// @see http://svn.saurik.com/repos/menes/trunk/aspectivec/AspectiveC.mm
-// @see https://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/ObjCRuntimeGuide/Articles/ocrtTypeEncodings.html
-bool logArgument(FILE *file, const char *type, va_list &args) {
-loop:
-  switch(*type) {
-    case '#': // A class object (Class).
-    case '@': { // An object (whether statically typed or typed id).
-      id value = va_arg(args, id);
-      logObject(file, value);
-    } break;
-    case ':': { // A method selector (SEL).
-      SEL value = va_arg(args, SEL);
-      if (value == NULL) {
-        fprintf(file, "NULL");
-      } else {
-        fprintf(file, "@selector(%s)", sel_getName(value));
-      }
-    } break;
-    case '*': { // A character string (char *).
-      const char *value = va_arg(args, const char *);
-      fprintf(file, "\"%s\"", value);
-    } break;
-    case '^': { // A pointer to type (^type).
-      void *value = va_arg(args, void *);
-      if (value == NULL) {
-        fprintf(file, "NULL");
-      } else {
-        fprintf(file, "0x%08lx", reinterpret_cast<uintptr_t>(value));
-      }
-    } break;
-    case 'B': { // A C++ bool or a C99 _Bool.
-      bool value = va_arg(args, int);
-      fprintf(file, "%s", value ? "true" : "false");
-    } break;
-    case 'c': { // A char.
-      signed char value = va_arg(args, int);
-      fprintf(file, "%d", value);
-    } break;
-    case 'C': { // An unsigned char.
-      unsigned char value = va_arg(args, unsigned int);
-      fprintf(file, "%d", value);
-    } break;
-    case 's': { // A short.
-      short value = va_arg(args, int);
-      fprintf(file, "%d", value);
-    } break;
-    case 'S': { // An unsigned short.
-      unsigned short value = va_arg(args, unsigned int);
-      fprintf(file, "%u", value);
-    } break;
-    case 'i': { // An int.
-      int value = va_arg(args, int);
-      if (value == INT_MAX) {
-        fprintf(file, "INT_MAX");
-      } else {
-        fprintf(file, "%d", value);
-      }
-    } break;
-    case 'I': { // An unsigned int.
-      unsigned int value = va_arg(args, unsigned int);
-      fprintf(file, "%u", value);
-    } break;
-    case 'l': { // A long.
-      long value = va_arg(args, long);
-      fprintf(file, "%ld", value);
-    } break;
-    case 'L': { // An unsigned long.
-      unsigned long value = va_arg(args, unsigned long);
-      fprintf(file, "%lu", value);
-    } break;
-    case 'q': { // A long long.
-      long long value = va_arg(args, long long);
-      fprintf(file, "%lld", value);
-    } break;
-    case 'Q': { // An unsigned long long.
-      unsigned long long value = va_arg(args, unsigned long long);
-      fprintf(file, "%llu", value);
-    } break;
-    case 'f': { // A float.
-      union {
-        uint32_t i;
-        float f;
-      } value = {va_arg(args, uint32_t)};
-      fprintf(file, "%g", value.f);
-    } break;
-    case 'd': { // A double.
-      double value = va_arg(args, double);
-      fprintf(file, "%g", value);
-    } break;
-    case '{': { // A struct. We check for some common structs.
-      if (strncmp(type, "{CGAffineTransform=", 19) == 0) {
-        CGAffineTransform at = va_arg(args, CGAffineTransform);
-        logNSStringForStruct(file, NSStringFromCGAffineTransform(at));
-      } else if (strncmp(type, "{CGPoint=", 9) == 0) {
-        CGPoint point = va_arg(args, CGPoint);
-        logNSStringForStruct(file, NSStringFromCGPoint(point));
-      } else if (strncmp(type, "{CGRect=", 8) == 0) {
-        CGRect rect = va_arg(args, CGRect);
-        logNSStringForStruct(file, NSStringFromCGRect(rect));
-      } else if (strncmp(type, "{CGSize=", 8) == 0) {
-        CGSize size = va_arg(args, CGSize);
-        logNSStringForStruct(file, NSStringFromCGSize(size));
-      } else if (strncmp(type, "{UIEdgeInsets=", 14) == 0) {
-        UIEdgeInsets insets = va_arg(args, UIEdgeInsets);
-        logNSStringForStruct(file, NSStringFromUIEdgeInsets(insets));
-      } else if (strncmp(type, "{UIOffset=", 10) == 0) {
-        UIOffset offset = va_arg(args, UIOffset);
-        logNSStringForStruct(file, NSStringFromUIOffset(offset));
-      } else if (strncmp(type, "{_NSRange=", 10) == 0) {
-        NSRange range = va_arg(args, NSRange);
-        logNSStringForStruct(file, NSStringFromRange(range));
-      } else { // Nope.
-        return false;
-      }
-    } break;
-    case 'N': // inout.
-    case 'n': // in.
-    case 'O': // bycopy.
-    case 'o': // out.
-    case 'R': // byref.
-    case 'r': // const.
-    case 'V': // oneway.
-      ++type;
-      goto loop;
-    default:
-      return false;
-  }
-  return true;
-}
-#endif
